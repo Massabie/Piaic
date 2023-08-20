f59596e3cdd41ec904d3cdb171a9ac4059a616c9
Massabie <m.massab4268@gmail.com>
2023-08-20 05:28:15 -0700

    massab

---
+++ file.ts
@@ -0,0 +1,12 @@
+//temperatre Celcius to Farenhite converter and vice vrca
+var temperature:number = 37;
+var CtoF = temperature*1.8+32;
+
+console.log(`Celcius to Farenhite ${CtoF}`);
+
+var FtoC = (temperature - 32) * 5/9;
+console.log(`Farenhite to celcius ${FtoC}`)
+
+for(let i=1; i<=100; i++){
+    console.log("i",i)
+}

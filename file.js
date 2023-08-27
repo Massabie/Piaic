359a20e937146f875ce846c3d03ade1c89c0cb61
Massabie <m.massab4268@gmail.com>
2023-08-27 02:32:37 -0700

    first commit

--- file.js
+++ file.js
@@ -1,10 +1,37 @@
 "use strict";
-//temperatre Celcius to Farenhite converter and vice vrca
-var temperature = 37;
-var CtoF = temperature * 1.8 + 32;
-console.log(`Celcius to Farenhite ${CtoF}`);
-var FtoC = (temperature - 32) * 5 / 9;
-console.log(`Farenhite to celcius ${FtoC}`);
-for (let i = 1; i <= 100; i++) {
-    console.log("i", i);
+var num = 22;
+var num2 = 44;
+var resolt = 22 < 44;
+console.log(resolt);
+// var useName:any = "massab";
+// var age:any=19
+// var Nationality :any ="faisalabad";
+// var result:any =age>19 && useName=="massab";
+// console.log(result);
+// function cTofTemperature (tempF:any){
+//  var resultf :any =tempF*(9/5)+32;
+// console.log(resultf);
+//  }
+//  cTofTemperature(39);
+//  cTofTemperature(58);
+// function percentage (numbers:any,total:any){
+// var result:any = (numbers*100)/total ;
+// console.log(result);
+// }
+// percentage(347,500);
+// function sum (num1:any,num2:any){
+// var result:any= (num1+num2);
+// return result;
+// }
+// var addResult:any=sum(50,40);
+// addResult +=5;
+// console.log(addResult);
+var studentsNames = [`massab`, `haris`, `saad`, `shoaib`, `osama`];
+console.log(studentsNames.length);
+function pushnewiteminarray(studentsName) {
+    studentsNames[studentsNames.length] = studentsName;
 }
+pushnewiteminarray(`hammad`);
+pushnewiteminarray(`zeshan`);
+pushnewiteminarray(`zufishan`);
+console.log(studentsNames);
--- file.ts
+++ file.ts
@@ -1,12 +1,49 @@
-//temperatre Celcius to Farenhite converter and vice vrca
-var temperature:number = 37;
-var CtoF = temperature*1.8+32;
 
-console.log(`Celcius to Farenhite ${CtoF}`);
+ var num:any=22;
+ var num2:any=44;
+ var resolt:any= 22<44;
+ console.log(resolt);
 
-var FtoC = (temperature - 32) * 5/9;
-console.log(`Farenhite to celcius ${FtoC}`)
+// var useName:any = "massab";
+// var age:any=19
+// var Nationality :any ="faisalabad";
+// var result:any =age>19 && useName=="massab";
+// console.log(result);
 
-for(let i=1; i<=100; i++){
-    console.log("i",i)
+
+// function cTofTemperature (tempF:any){
+//  var resultf :any =tempF*(9/5)+32;
+// console.log(resultf);
+//  }
+
+//  cTofTemperature(39);
+//  cTofTemperature(58);
+
+// function percentage (numbers:any,total:any){
+// var result:any = (numbers*100)/total ;
+// console.log(result);
+// }
+
+// percentage(347,500);
+
+// function sum (num1:any,num2:any){
+// var result:any= (num1+num2);
+// return result;
+// }
+
+
+// var addResult:any=sum(50,40);
+// addResult +=5;
+// console.log(addResult);
+var studentsNames:any []=[`massab`,`haris`,`saad`,`shoaib`,`osama`]
+console.log(studentsNames.length);
+function pushnewiteminarray(studentsName:any){
+studentsNames[studentsNames.length] = studentsName
+
 }
+pushnewiteminarray(`hammad`)
+pushnewiteminarray(`zeshan`)
+pushnewiteminarray(`zufishan`)
+
+console.log(studentsNames);
+
